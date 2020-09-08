65ff0042283836b42f31284aa15b9882d7f83260
itsgitz <anggit.ginanjar@outlook.com>
2020-09-08 18:19:56 +0700

    Try to using environment variables that loaded by dotenv package

--- class/class.database.php
+++ class/class.database.php
@@ -1,11 +1,33 @@
 <?php
 
-namespace Madam\DB;
+namespace Madam;
+
 
 class Database
 {
+    private $servername;
+    private $username;
+    private $password;
+
     function __construct()
     {
+        $this->servername = $_ENV['DB_SERVER'];
+        $this->username = $_ENV['DB_USERNAME'];
+        $this->password = $_ENV['DB_PASSWORD'];
+    }
 
+    public function getServername()
+    {
+        return $this->servername;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
     }
 }
