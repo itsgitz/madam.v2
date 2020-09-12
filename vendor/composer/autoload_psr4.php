3bfa20c036ae703862cc96bf6f6901b3ea3c73dc
itsgitz <anggit.ginanjar@outlook.com>
2020-09-12 12:13:42 +0700

    Update composer packages

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,6 +6,8 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'eftec\\bladeone\\' => array($vendorDir . '/eftec/bladeone/lib'),
     'Symfony\\Component\\Dotenv\\' => array($vendorDir . '/symfony/dotenv'),
-    'Madam\\' => array($baseDir . '/class'),
+    'Madam\\' => array($baseDir . '/class', $baseDir . '/app', $baseDir . '/controllers', $baseDir . '/router'),
+    'Klein\\' => array($vendorDir . '/klein/klein/src/Klein'),
 );
