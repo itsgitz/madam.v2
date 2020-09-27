8cfebe63e7feb38432cbf777d8b6cd4c381998d6
itsgitz <anggit.ginanjar@outlook.com>
2020-09-27 08:14:31 +0700

    Add Http class

---
+++ app/Http.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Author: anggit.ginanjar@outlook.com <itsgitz.com>
+ */
+
+namespace Madam;
+
+
+class Http
+{
+    const METHOD_GET = 'GET';
+    const METHOD_POST = 'POST';
+    const METHOD_PUT = 'PUT';
+    const METHOD_DELETE = 'DELETE';
+}
