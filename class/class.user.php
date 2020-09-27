51625b8948f84e273132f6d2c2f74bcef6fa347d
itsgitz <anggit.ginanjar@outlook.com>
2020-09-27 22:23:48 +0700

    Add new method getUsers, getUserById, and getUserByUsername

--- class/class.user.php
+++ class/class.user.php
@@ -5,11 +5,56 @@
 
 namespace Madam;
 
-
 class User
 {
+    private $table;
+    private $db;
+
     function __construct()
     {
+        $this->db = new Database();
+        $this->table = $_ENV['USER_TABLE'];
+    }
 
+    public function getUsers()
+    {
+        $query  = "SELECT * FROM {$this->table}";
+        $result = $this->db->getConnection()->query($query);
+
+        if ($result->num_rows > 0) {
+            return $result->fetch_assoc();
+        } else {
+            return null;
+        }
+    }
+
+    public function getUserById($id)
+    {
+        $stmt = $this->db->getConnection()->prepare("SELECT * FROM {$this->table} WHERE id = ?");
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            return $result->fetch_assoc();
+        } else {
+            return null;
+        }
+    }
+
+    public function getUserByUsername($username)
+    {   
+        $stmt = $this->db->getConnection()->prepare("SELECT * FROM {$this->table} WHERE username = ?");
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        if ($result->num_rows == 1) {
+            return $result->fetch_assoc();
+        } else {
+            return null;
+        }
     }
 }
