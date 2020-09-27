54f65a16e427a108c2f552ffdb22bc2d0f81a060
itsgitz <anggit.ginanjar@outlook.com>
2020-09-27 22:25:31 +0700

    Add error message template bind variable if error occured

--- views/login.blade.php
+++ views/login.blade.php
@@ -5,7 +5,7 @@
     </head>
     <body>
         <h1>{{$title}}</h1>
-
+        <h3>{{$error_message}}</h3>
         <form method="POST" action="/login">
             <input type="text" name="username" placeholder="username">
             <input type="password" name="password" placeholder="password">
