005a6ac564d8b535f04ce1bb11b103a35db475d8
itsgitz <anggit.ginanjar@outlook.com>
2020-10-05 00:23:17 +0700

    Add user_role and admin value

--- app/Sessions.php
+++ app/Sessions.php
@@ -14,12 +14,21 @@ class Sessions
         
     }
 
-    public function setSessions($id, $username, $name, $email)
+    public function setSessions(
+        $id,
+        $username,
+        $name,
+        $email,
+        $userRole,
+        $admin
+    )
     {
         $_SESSION['id'] = $id;
         $_SESSION['username'] = $username;
         $_SESSION['name'] = $name;
         $_SESSION['email'] = $email;
+        $_SESSION['user_role'] = $userRole;
+        $_SESSION['admin'] = $admin;
     }
 
     public function getSessions()
@@ -28,7 +37,9 @@ class Sessions
             'id' => $_SESSION['id'],
             'username' => $_SESSION['username'],
             'name' => $_SESSION['name'],
-            'email' => $_SESSION['email']
+            'email' => $_SESSION['email'],
+            'user_role' => $_SESSION['user_role'],
+            'admin' => $_SESSION['admin']
         ];
     }
 
