84b191a537c80274f822a431f49b5f85c1ff3d41
itsgitz <anggit.ginanjar@outlook.com>
2020-11-15 12:50:56 +0700

    Add definition (constant value) for request proccessing.

--- app/Http.php
+++ app/Http.php
@@ -13,4 +13,8 @@ class Http
     const METHOD_POST = 'POST';
     const METHOD_PUT = 'PUT';
     const METHOD_DELETE = 'DELETE';
+    const ADD_REQUEST = 'add';
+    const REMOVE_REQUEST = 'remove';
+    const EDIT_REQUEST = 'edit';
+    const SEARCH_REQUEST = 'search';
 }
