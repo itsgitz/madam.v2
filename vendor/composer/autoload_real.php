8b605e998a706f3cfbabe00007beb1d370da94ff
itsgitz <anggit.ginanjar@outlook.com>
2020-11-28 00:41:15 +0700

    Resolved conflict.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInitb159af37f093bcd1eef5562a49cc01e7
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitb159af37f093bcd1eef5562a49cc01e7', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitb159af37f093bcd1eef5562a49cc01e7', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitb159af37f093bcd1eef5562a49cc01e7::getInitializer($loader));
         } else {
