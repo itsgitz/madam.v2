b118e2e77876214c698cdeab7d804cf889fc7970
itsgitz <anggit.ginanjar@outlook.com>
2020-12-27 22:35:21 +0700

    Add VLAN site functions.

--- assets/js/networking.js
+++ assets/js/networking.js
@@ -1,4 +1,10 @@
-$(function() {
+$(function () {
+    vlanGroup();
+
+    vlanSite();
+});
+
+function vlanGroup() {
     let removeModal = $('#removeModal'),
         editModal = $('#editModal'),
         vlanGroupName = $('#vlanGroupName'),
@@ -35,4 +41,61 @@ $(function() {
         vlanGroupNameEdit.val(name)
         vlanGroupSiteEdit.val(site)
     });
-});
+}
+
+function vlanSite() {
+    let removeVlanModal = $('#removeVlanModal'),
+        vlanId = $('#vlanId'),
+        vlanVlanId = $('#vlanVlanId'),
+        vlanPrefixes = $('#vlanPrefixes'),
+        vlanTenant = $('#vlanTenant'),
+        vlanStatus = $('#vlanStatus'),
+        vlanRole = $('#vlanRole');
+
+    let editVlanModal = $('#editVlanModal'),
+        vlanIdEdit = $('#vlanIdEdit'),
+        vlanVlanIdEdit = $('#vlanVlanIdEdit'),
+        vlanPrefixesEdit = $('#vlanPrefixesEdit'),
+        vlanTenantEdit = $('#vlanTenantEdit'),
+        vlanStatusEdit = $('#vlanStatusEdit'),
+        vlanRoleEdit = $('#vlanRoleEdit'),
+        vlanDescriptionEdit = $('#vlanDescriptionEdit');
+
+    removeVlanModal.on('show.bs.modal', function (e) {
+        let idData = $(e.relatedTarget).data('id'),
+            vlanIdData = $(e.relatedTarget).data('vlan-id'),
+            prefixesData = $(e.relatedTarget).data('prefixes'),
+            tenantData = $(e.relatedTarget).data('tenant'),
+            statusData = $(e.relatedTarget).data('status'),
+            roleData = $(e.relatedTarget).data('role');
+
+        console.log('id:', idData, 'vlanId:', vlanIdData, 'prefixes:', prefixesData, 'tenant:', tenantData, 'status:', statusData, 'role:', roleData);
+
+        vlanId.val(idData)
+        vlanVlanId.text(vlanIdData)
+        vlanPrefixes.text(prefixesData)
+        vlanTenant.text(tenantData)
+        vlanStatus.text(statusData)
+        vlanRole.text(roleData)
+    });
+
+    editVlanModal.on('show.bs.modal', function (e) {
+        let idData = $(e.relatedTarget).data('id'),
+            vlanIdData = $(e.relatedTarget).data('vlan-id'),
+            prefixesData = $(e.relatedTarget).data('prefixes'),
+            tenantData = $(e.relatedTarget).data('tenant'),
+            statusData = $(e.relatedTarget).data('status'),
+            roleData = $(e.relatedTarget).data('role'),
+            descriptionData = $(e.relatedTarget).data('description');
+
+        console.log('id:', idData, 'vlanId:', vlanIdData, 'prefixes:', prefixesData, 'tenant:', tenantData, 'status:', statusData, 'role:', roleData, 'description:', descriptionData);
+        
+        vlanIdEdit.val(idData)
+        vlanVlanIdEdit.val(vlanIdData)
+        vlanPrefixesEdit.val(prefixesData)
+        vlanTenantEdit.val(tenantData)
+        vlanStatusEdit.val(statusData)
+        vlanRoleEdit.val(roleData)
+        vlanDescriptionEdit.val(descriptionData)
+    });
+}
